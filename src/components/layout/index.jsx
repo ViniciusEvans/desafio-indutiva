909746322d6c8c54072556d9f68a3a2e38b8f393
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 15:46:01 -0300

    adicionando estilo

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -2,32 +2,39 @@ import { Outlet } from "react-router-dom";
 import NavBar from "../navBar";
 import { Container, Row, Col } from "react-bootstrap";
 import Header from "../header";
+import { UserProvider } from "../../context/userContext";
 function Layout() {
   return (
-    <Container fluid className="app-background">
-      <Row className="app-overall-size">
-        <Col sm={1} className="navBar-container px-0">
-          <NavBar />
-        </Col>
-        <Col sm={11}>
-          <Container
-            fluid
-            style={{ background: "#E5E5E5", minHeight: "100vh" }}
-          >
-            <Row>
-              <Col>
-                <Header />
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Outlet />
-              </Col>
-            </Row>
-          </Container>
-        </Col>
-      </Row>
-    </Container>
+    <UserProvider>
+      <Container fluid className="app-background">
+        <Row className="app-overall-size">
+          <Col sm={1} className="navBar-container px-0">
+            <NavBar />
+          </Col>
+          <Col sm={11} style={{ padding: "0" }}>
+            <Container
+              fluid
+              style={{
+                background: "#E5E5E5",
+                minHeight: "100vh",
+                padding: "0",
+              }}
+            >
+              <Row>
+                <Col>
+                  <Header />
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  <Outlet />
+                </Col>
+              </Row>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </UserProvider>
   );
 }
 
