06fa9ceedddcb1eb7574cd3097b04e426fa73373
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 14:36:13 -0300

    definindo rotas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-
-function Routes() {
+import Layout from "../components/layout";
+import Products from "../pages/products";
+import NewProducts from "../pages/newProduct";
+function Rotas() {
   return (
     <Routes>
-      <Route />
+      <Route path={"/"} element={<Layout />}>
+        <Route path="new-products" element={<Products />} />
+        <Route path="products" element={<NewProducts />} />
+      </Route>
     </Routes>
   );
 }
 
-export default Routes;
+export default Rotas;
