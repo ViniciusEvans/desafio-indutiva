0f284e977ba08f467e6cb04019050b826bb3aa5e
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 15:45:32 -0300

    adicionando titulo do header dinâmico

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,10 @@
+import "./style.scss";
+import useUser from "../../hooks/userUser";
 function Header() {
+  const { title } = useUser();
   return (
     <div className="header">
-      <h1>header</h1>
+      <h1>{title ? title : "Produtos"}</h1>
     </div>
   );
 }
