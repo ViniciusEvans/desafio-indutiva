cd3bedd8f45ce3e93a9fa8c99916a7cab2781342
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 15:46:40 -0300

    adicionando funcionalidade de título dinâmico e refatoração

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -1,18 +1,25 @@
 import { NavLink } from "react-router-dom";
 import newProductsIcon from "../../assets/newProductsIcon.svg";
 import productsIcon from "../../assets/productsIcon.svg";
-
+import useUser from "../../hooks/userUser";
+import "./style.scss";
 function NavBar() {
-  const stylesheet = { display: "flex", flexDirection: "column" };
+  const { setTitle } = useUser();
+  function handleSetTitle(title) {
+    setTitle(title);
+  }
   return (
-    <div className="navbar" style={stylesheet}>
-      <div className="link-1">
-        <NavLink to={"new-products"}>
+    <div className="navbar">
+      <div className="link link-1">
+        <NavLink
+          onClick={() => handleSetTitle("Cadastre novo produto")}
+          to={"new-products"}
+        >
           <img src={newProductsIcon} alt="new product" />
         </NavLink>
       </div>
-      <div className="link-1">
-        <NavLink to={"products"}>
+      <div className="link link-2">
+        <NavLink onClick={() => handleSetTitle("Produtos")} to={""}>
           <img src={productsIcon} alt="products" />
         </NavLink>
       </div>
