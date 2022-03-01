907d09335b46e5864e58dde88ced1efb4dc8e177
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-01 11:49:03 -0300

    adicionando features ao products

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,9 +1,24 @@
-import { useState } from "react";
-import { Table, Container, Row, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, Modal, Table } from "react-bootstrap";
+import deleteIcon from "../../assets/deleteIcon.svg";
+import editIcon from "../../assets/editIcon.svg";
+import useUser from "../../hooks/userUser";
 import "./style.scss";
 
 function Products() {
+  const { setAddOrEdit, notify } = useUser();
   const [products, setProducts] = useState([]);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [id, setId] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    handleGetProducts();
+    setAddOrEdit("");
+  }, [toggle]);
+
   function formatToCurrency(inputNumber) {
     const convertedValue = new Intl.NumberFormat("pt-BR", {
       style: "currency",
@@ -11,7 +26,48 @@ function Products() {
     }).format(inputNumber);
     return convertedValue;
   }
+  async function handleGetProducts() {
+    try {
+      const response = await fetch(
+        "https://db-indutiva.herokuapp.com/getProducts",
+        {
+          method: "GET",
+          headers: { "content-type": "application/json" },
+        }
+      );
+      const data = await response.json();
+      setProducts(data);
+      setToggle(!toggle);
+    } catch (error) {
+      notify(error.message, "error");
+    }
+  }
 
+  function handleEditProduct(id) {
+    setAddOrEdit(["edit", id]);
+    navigate("/new-products");
+  }
+
+  async function handleDeleteProduct(id) {
+    try {
+      const response = await fetch(
+        "https://db-indutiva.herokuapp.com/deleteProducts/" + id,
+        {
+          method: "DELETE",
+          headers: { "content-type": "application/json" },
+        }
+      );
+      const data = await response.json();
+      setToggle(!toggle);
+      setTimeout(() => setDeleteModal(false), 2000);
+      if (data.error) {
+        notify(data.error, "error");
+      }
+      notify(data.success, "success");
+    } catch (error) {
+      notify(error.error, "error");
+    }
+  }
   return (
     <Container>
       <Table striped hover>
@@ -22,6 +78,8 @@ function Products() {
             <th>imagem</th>
             <th>Valor</th>
             <th>Descrição</th>
+            <th></th>
+            <th></th>
           </tr>
         </thead>
         <tbody>
@@ -30,14 +88,58 @@ function Products() {
               <td>{item.id}</td>
               <td>{item.title}</td>
               <td>
-                <img src={item.img} />
+                <img src={item.image} />
               </td>
-              <td>{item.amount}</td>
-              <td>{item.desc}</td>
+              <td>{formatToCurrency(item.amount)}</td>
+              <td>{item.description}</td>
+              <td>
+                <img
+                  onClick={() => handleEditProduct(item.id)}
+                  src={editIcon}
+                  alt="edit"
+                />
+              </td>
+              <td>
+                <img
+                  onClick={() => {
+                    setDeleteModal(true);
+                    setId(item.id);
+                  }}
+                  src={deleteIcon}
+                  alt="delete"
+                />
+              </td>
             </tr>
           ))}
         </tbody>
       </Table>
+      <Modal
+        size="sm"
+        show={deleteModal}
+        onHide={() => setDeleteModal(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            Você quer deletar esse produto?
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ display: "flex", justifyContent: "space-around" }}>
+          <button
+            className="btn btn-confirm"
+            onClick={() => handleDeleteProduct(id)}
+          >
+            sim
+          </button>
+          <button
+            className="btn btn-cancel"
+            onClick={() => setDeleteModal(false)}
+          >
+            não
+          </button>
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 }
