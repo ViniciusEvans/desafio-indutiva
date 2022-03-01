4104597e7789d0590ab2776d451abaf9fdc35f77
Vinicius <vinibrother.evansz2@gmail.com>
2022-02-28 23:45:07 -0300

    criando tabela de produtos

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,8 +1,44 @@
+import { useState } from "react";
+import { Table, Container, Row, Col } from "react-bootstrap";
+import "./style.scss";
+
 function Products() {
+  const [products, setProducts] = useState([]);
+  function formatToCurrency(inputNumber) {
+    const convertedValue = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(inputNumber);
+    return convertedValue;
+  }
+
   return (
-    <div>
-      <h1>test1</h1>
-    </div>
+    <Container>
+      <Table striped hover>
+        <thead>
+          <tr>
+            <th>id</th>
+            <th>Título</th>
+            <th>imagem</th>
+            <th>Valor</th>
+            <th>Descrição</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item) => (
+            <tr>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>
+                <img src={item.img} />
+              </td>
+              <td>{item.amount}</td>
+              <td>{item.desc}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
