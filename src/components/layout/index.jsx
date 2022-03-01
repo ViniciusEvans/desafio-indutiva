ba9c6bf23c45ac56db0017ae2546f5a14b6cfae8
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-01 11:47:49 -0300

    colocando contexto no layout

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,40 +1,52 @@
+import { Col, Container, Row } from "react-bootstrap";
 import { Outlet } from "react-router-dom";
-import NavBar from "../navBar";
-import { Container, Row, Col } from "react-bootstrap";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "../header";
-import { UserProvider } from "../../context/userContext";
+import NavBar from "../navBar";
+
 function Layout() {
   return (
-    <UserProvider>
-      <Container fluid className="app-background">
-        <Row className="app-overall-size" style={{ marginRight: "0" }}>
-          <Col sm={1} className="navBar-container px-0">
-            <NavBar />
-          </Col>
-          <Col sm={11} style={{ padding: "0" }}>
-            <Container
-              fluid
-              style={{
-                background: "#E5E5E5",
-                minHeight: "100%",
-                padding: "0",
-              }}
-            >
-              <Row>
-                <Col>
-                  <Header />
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <Outlet />
-                </Col>
-              </Row>
-            </Container>
-          </Col>
-        </Row>
-      </Container>
-    </UserProvider>
+    <Container fluid className="app-background">
+      <Row className="app-overall-size" style={{ marginRight: "0" }}>
+        <Col sm={1} className="navBar-container px-0">
+          <NavBar />
+        </Col>
+        <Col sm={11} style={{ padding: "0" }}>
+          <Container
+            fluid
+            style={{
+              background: "#E5E5E5",
+              minHeight: "100%",
+              padding: "0",
+            }}
+          >
+            <Row>
+              <Col>
+                <Header />
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <ToastContainer
+                  position="top-right"
+                  autoClose={5000}
+                  hideProgressBar={false}
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                />
+                <ToastContainer />
+                <Outlet />
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
