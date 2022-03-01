f39af2abfccb51b55ac058c8b97242bc4321f384
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-01 11:49:50 -0300

    adicionando provider ao routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "../components/layout";
 import Products from "../pages/products";
+import { UserProvider } from "../context/userContext";
 import NewProducts from "../pages/newProduct";
 function Rotas() {
   return (
-    <Routes>
-      <Route path={"/"} element={<Layout />}>
-        <Route path="" element={<Products />} />
-        <Route path="new-products" element={<NewProducts />} />
-      </Route>
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path={"/"} element={<Layout />}>
+          <Route path="" element={<Products />} />
+          <Route path="new-products" element={<NewProducts />} />
+        </Route>
+      </Routes>
+    </UserProvider>
   );
 }
 
