eb16ce599b72bc715b286056a3fe7ee15f701880
Vinicius <vinibrother.evansz2@gmail.com>
2022-03-01 11:48:15 -0300

    adicionando valores ao hoook de porver contexto

--- src/hooks/useUserProvider.jsx
+++ src/hooks/useUserProvider.jsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 function useUserProvider() {
   const [title, setTitle] = useState("");
-  return { title, setTitle };
+  const [addOrEdit, setAddOrEdit] = useState([]);
+  const notify = (message, type) =>
+    toast[type](message, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  return {
+    addOrEdit,
+    setAddOrEdit,
+    notify,
+    title,
+    setTitle,
+  };
 }
 
 export default useUserProvider;
